49ff784dcfa4cd6a4c1707c55b3cd108ba9f0258
Shubham Soni <your-email@gmail.com>
2026-05-22 16:57:52 +0530

    Initial Commit

---
+++ ai.py
@@ -0,0 +1,46 @@
+import google.generativeai as genai
+
+# Configure API Key
+genai.configure(api_key="AIzaSyDHDZpFJEmvXEI6YLceR8z6K_gFgBjwBkw")
+
+# Gemini Model
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+
+# ================= VEHICLE RECOMMENDATION =================
+
+def recommend_vehicle(budget, passengers, trip_type):
+
+    prompt = f"""
+    Suggest the best rental vehicle.
+
+    Budget: {budget}
+    Passengers: {passengers}
+    Trip Type: {trip_type}
+
+    Give:
+    1. Vehicle Name
+    2. Short Reason
+    """
+
+    response = model.generate_content(prompt)
+
+    return response.text
+
+
+# ================= CHATBOT =================
+
+def chatbot_response(user_message):
+
+    prompt = f"""
+    You are an AI assistant for a vehicle rental platform called Rent A Ride.
+
+    User Question:
+    {user_message}
+
+    Answer shortly and professionally.
+    """
+
+    response = model.generate_content(prompt)
+
+    return response.text
